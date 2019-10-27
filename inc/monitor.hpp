b38b0dac64269933818dcdfc13f1f9c2eb63ba9c
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-10-27 06:34:00 -0400

    Added key to rule

--- inc/monitor.hpp
+++ inc/monitor.hpp
@@ -8,30 +8,34 @@
 #define MONITOR_HPP
 
 #include <libaudit.h>
+#include <string>
 
 class IDirMonitor {
-	virtual int init() = 0;
-	virtual int add_dir(const char *dir) = 0;
+  virtual int init() = 0;
+  virtual int add_dir(const std::string &dir) = 0;
 };
 
 class LinuxAudit : IDirMonitor {
-	int fd;
-	struct audit_rule_data *rule;
+  int fd;
+  struct audit_rule_data *rule;
+
+  int add_key();
 
 public:
-	LinuxAudit() : fd(-1), rule(0) {}
-	~LinuxAudit() {
-		// Delete created rule
-		if ((fd >= 0) && (rule != 0))
-			audit_delete_rule_data(fd, rule, AUDIT_FILTER_EXIT, AUDIT_ALWAYS);
-		if (fd >= 0)
-			audit_close(fd);
-		if (rule != 0)
-			free(rule);
-	}
+  static const std::string FILTER_KEY;
+  LinuxAudit() : fd(-1), rule(0) {}
+  ~LinuxAudit() {
+    // Delete created rule
+    if ((fd >= 0) && (rule != 0))
+      audit_delete_rule_data(fd, rule, AUDIT_FILTER_EXIT, AUDIT_ALWAYS);
+    if (fd >= 0)
+      audit_close(fd);
+    if (rule != 0)
+      free(rule);
+  }
 
-	int init() override ;
-	int add_dir(const char *dir) override;
+  int init() override;
+  int add_dir(const std::string &dir) override;
 };
 
 #endif
--- src/monitor.cpp
+++ src/monitor.cpp
@@ -4,56 +4,69 @@
 /// @version  0.0
 /// @date Oct 26 2019
 
-#include <libaudit.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <libaudit.h>
 #include <locale.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <string>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
 
 #include "monitor.hpp"
 
+const std::string LinuxAudit::FILTER_KEY = "file-monitor";
+
 int LinuxAudit::init() {
   rule = reinterpret_cast<audit_rule_data *>(malloc(sizeof(audit_rule_data)));
-	if (!rule) {
-		syslog(LOG_ERR, "Failed to allocate data");
-		return -1;
-	}
+  if (!rule) {
+    syslog(LOG_ERR, "Failed to allocate data");
+    return -1;
+  }
   memset(&rule, 0, sizeof(rule));
 
-	fd = audit_open();
-	if (fd < 0) {
-		syslog(LOG_ERR, "Failed to open communication with netlink");
-		return -2;
-	}
+  fd = audit_open();
+  if (fd < 0) {
+    syslog(LOG_ERR, "Failed to open communication with netlink");
+    return -2;
+  }
 
-	if (audit_is_enabled(fd) != 1) {
-		syslog(LOG_ERR, "auditd is not enabled");
-		return -3;
-	}
+  if (audit_is_enabled(fd) != 1) {
+    syslog(LOG_ERR, "auditd is not enabled");
+    return -3;
+  }
 
   return 0;
 }
 
-int LinuxAudit::add_dir(const char *dir) {
-	if (audit_add_dir(&rule, dir) < 0) {
-		syslog(LOG_ERR, "Failed to add watch to dir: '%s'", dir);
-		return -1;
-	}
+int LinuxAudit::add_dir(const std::string &dir) {
+  if (dir.empty()) {
+    syslog(LOG_ERR, "Invalid dir argument");
+    return -1;
+  }
+  if (audit_add_dir(&rule, dir.c_str()) < 0) {
+    syslog(LOG_ERR, "Failed to add watch to dir: '%s'", dir.c_str());
+    return -2;
+  }
 
-	/// There does not seem a way to get this rule. Just try to delete ahead of 
-	/// time, in case it escaped us before
-	audit_delete_rule_data(fd, rule, AUDIT_FILTER_EXIT, AUDIT_ALWAYS);
+  std::string key = "key=" + FILTER_KEY;
+  if (audit_rule_fieldpair_data(&rule, key.c_str(), AUDIT_ALWAYS) != 0) {
+    syslog(LOG_ERR, "Failed to add key: '%s', to rule", key.c_str());
+    return -3;
+  }
 
-	if (audit_add_rule_data(fd, rule, AUDIT_FILTER_EXIT, AUDIT_ALWAYS) < 0) {
-		syslog(LOG_ERR, "Failed to add rule to audit");
-		return -2;
-	}
+  /// There does not seem a way to get this rule. Just try to delete ahead of
+  /// time, in case it escaped us before
+  audit_delete_rule_data(fd, rule, AUDIT_FILTER_EXIT, AUDIT_ALWAYS);
 
-	return 0;
+  if (audit_add_rule_data(fd, rule, AUDIT_FILTER_EXIT, AUDIT_ALWAYS) < 0) {
+    syslog(LOG_ERR, "Failed to add rule to audit");
+    return -4;
+  }
+
+  return 0;
 }
